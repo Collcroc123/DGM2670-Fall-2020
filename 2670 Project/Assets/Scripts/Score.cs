81d488dbc98e428af2342aaccffb1d9c2c75978f
Collin Gale <Collin.g086@gmail.com>
2020-08-26 14:17:31 -0600

    Datas and Scores

---
+++ 2670 Project/Assets/Scripts/Score.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public IntData score;
+
+    private void Start()
+    {
+        GetComponent<Text>().text = score.value.ToString();
+    }
+}
