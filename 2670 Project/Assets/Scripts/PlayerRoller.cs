7c52a3ea0927aea01e019ccd16e388ba2e3ae8ee
Collin Gale <Collin.g086@gmail.com>
2020-08-31 17:54:43 -0600

    Roller Ball

---
+++ 2670 Project/Assets/Scripts/PlayerRoller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoller : MonoBehaviour
+{
+    private Rigidbody ball;
+    private Vector3 movement;
+    private float currentSpeed = 2f;
+    private float gravity = 1f;
+    private float jumpForce = 50f;
+    
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+    }
+/*
+    void OnMove(InputValue movementValue)
+    {
+        Vector3 movementVector = movementValue.Get<Vector3>();
+    }*/
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Vertical")*currentSpeed;
+        movement.z = Input.GetAxis("Horizontal")* -currentSpeed;
+        ball.AddForce(movement);
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Jump();
+        }
+        
+        //movement.y -= gravity;
+    }
+
+    IEnumerator Jump()
+    {
+        movement.y = jumpForce;
+        yield return new WaitForSeconds(1f);
+        movement.y = 0f;
+    }
+}
