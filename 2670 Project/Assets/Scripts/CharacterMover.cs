88934fd8ff31fd78e862fb70624db075eaf4f564
Collin Gale <Collin.g086@gmail.com>
2020-08-31 22:14:56 -0600

    Terrain and Trails

--- 2670 Project/Assets/Scripts/CharacterMover.cs
+++ 2670 Project/Assets/Scripts/CharacterMover.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class CharacterMover : MonoBehaviour
@@ -8,21 +6,21 @@ public class CharacterMover : MonoBehaviour
     private CharacterController controller;
     private Vector3 movement;
     private Vector3 rotateMovement;
-    private float gravity = 4.9f;
+    private float gravity = 0.5f;
     private float currentSpeed = 5f;
     public float rotateSpeed = 5f;
     public float moveSpeed = 5f;
     public float sprintSpeed = 7.5f;
-    public float jumpForce = 50f;
-    public int maxJumps = 1;
-    public int jumpCount;
+    public float jumpForce = 5f;
+    //public int maxJumps = 1;
+    //public int jumpCount;
 
-    void Start()
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
     }
-    
-    void Update()
+
+    private void Update()
     {
         rotateMovement.y = Input.GetAxis("Mouse X")*rotateSpeed;
         transform.Rotate(rotateMovement);
@@ -38,14 +36,14 @@ public class CharacterMover : MonoBehaviour
             currentSpeed = moveSpeed;
         }
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            movement.y = jumpForce;
-        }
-        
         if (controller.isGrounded)
         {
             movement.y = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                movement.y = jumpForce;
+                Debug.Log("JUMPED!");
+            }
         }
         else
         {
--- 2670 Project/Assets/Scripts/PlayerRoller.cs
+++ 2670 Project/Assets/Scripts/PlayerRoller.cs
@@ -1,24 +1,19 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerRoller : MonoBehaviour
 {
     private Rigidbody ball;
     private Vector3 movement;
-    private float currentSpeed = 2f;
+    private float currentSpeed = 3f;
     private float gravity = 1f;
     private float jumpForce = 50f;
+    private bool grounded;
     
     void Start()
     {
         ball = GetComponent<Rigidbody>();
     }
-/*
-    void OnMove(InputValue movementValue)
-    {
-        Vector3 movementVector = movementValue.Get<Vector3>();
-    }*/
 
     void Update()
     {
@@ -30,14 +25,31 @@ public class PlayerRoller : MonoBehaviour
         {
             //Jump();
         }
-        
-        //movement.y -= gravity;
     }
-
+    
     IEnumerator Jump()
     {
         movement.y = jumpForce;
         yield return new WaitForSeconds(1f);
         movement.y = 0f;
     }
+    
+    /*
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Bababooey");
+        if(other.collider.CompareTag("Ground"))
+        {
+            movement.y = 0;
+            if (Input.GetButtonDown("Jump"))
+            {
+                movement.y = jumpForce;
+            }
+        }
+        else
+        {
+            movement.y -= gravity;
+        }
+    }*/
 }
+
