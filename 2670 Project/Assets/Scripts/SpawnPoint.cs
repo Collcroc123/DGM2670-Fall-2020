f06f97003a6df92acf088087845d13f46525a82d
Collin Gale <Collin.g086@gmail.com>
2020-09-12 22:43:28 -0600

    Extremley minor changes

--- 2670 Project/Assets/Scripts/SpawnPoint.cs
+++ 2670 Project/Assets/Scripts/SpawnPoint.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
     public Vector3Data vData;
+    public CharacterController charControl;
     
     //Set the vData from the position value
-    
-    
+    private void Update()
+    {
+        //vData = charControl.position;
+    }
 }
