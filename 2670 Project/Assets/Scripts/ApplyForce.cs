fe2ca8f9366409ed996e57586f23b688f1afb408
Collin Gale <Collin.g086@gmail.com>
2020-09-14 14:01:00 -0600

    Instancing

---
+++ 2670 Project/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rBody;
+    private float force = 300f;
+    
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        var forceDirection = new Vector3(force,0,0);
+        rBody.AddRelativeForce(forceDirection);
+    }
+}
---
+++ 2670 Project/Assets/Scripts/Instancer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Instancer : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var location = transform.position;
+            var rotation = new Vector3(0,0,0);
+            Instantiate(prefab, location, Quaternion.Euler(rotation));
+        }
+    }
+}
