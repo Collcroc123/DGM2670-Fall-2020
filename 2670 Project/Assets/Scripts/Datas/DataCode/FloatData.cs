c58c1f26c43f1552060d445337d9543b7b69d903
Collin Gale <Collin.g086@gmail.com>
2020-10-19 14:46:33 -0600

    String List

--- 2670 Project/Assets/Scripts/Datas/DataCode/FloatData.cs
+++ 2670 Project/Assets/Scripts/Datas/DataCode/FloatData.cs
@@ -1,12 +1,23 @@
-﻿using UnityEngine;
+﻿using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class FloatData : ScriptableObject
 {
     public float value;
 
+    public void SetValue(float number)
+    {
+        value = number;
+    }
     public void UpdateValue(float number)
     {
         value += number;
+    }
+
+    public void SetImageFillAmount(Image img)
+    {
+        img.fillAmount = value;
     }
 }
