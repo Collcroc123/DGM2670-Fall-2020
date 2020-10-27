d9e8455d7c4747caeb2bb47f04f45271c318c0fc
Collin Gale <Collin.g086@gmail.com>
2020-10-26 23:12:54 -0600

    Platforms

---
+++ 2670 Project/Assets/Scripts/AttachObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class AttachObject : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherTag = other.CompareTag("Platform");
+        if (otherTag)
+        {
+            transform.parent = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent = null;
+    }
+}
