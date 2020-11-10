56a334c3632a66f5985f2bfd73ead4b7a4382d2b
Collin Gale <Collin.g086@gmail.com>
2020-11-10 08:55:23 -0700

    Scripting

--- 2670 Project/Assets/Scripts/GameAction.cs
+++ 2670 Project/Assets/Scripts/GameAction.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
-public class GameAction : MonoBehaviour
+public class GameAction : ScriptableObject
 {
     public UnityAction action;
+    public UnityAction<Transform> transformAction;
 
     public void Raise()
     {
         action?.Invoke();
+    }
+
+    public void Raise(Transform obj)
+    {
+        transformAction(obj);
     }
 }
