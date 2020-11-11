b4847a98ec6b01a5e91f354313d1a66b04098265
Collin Gale <Collin.g086@gmail.com>
2020-11-11 15:19:28 -0700

    AI Stuff

---
+++ 2670 Project/Assets/Scripts/InstanceBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+public class InstanceBehaviour : MonoBehaviour
+{
+    public GameAction gameActionObj;
+    public GameObject prefab;
+    public float holdTime = 0.5f;
+    public bool canLoop;
+    public UnityEvent startEvent, onCallEvent;
+    private WaitForSeconds wfs;
+    
+    private void Start()
+    {
+        wfs = new WaitForSeconds(holdTime);
+        startEvent.Invoke();
+    }
+    
+    public void StartLoopEvents()
+    {
+        StartCoroutine(CallInstanceEvent());
+    }
+    
+    private IEnumerator CallInstanceEvent()
+    {
+        while (canLoop)
+        {
+            onCallEvent.Invoke();
+            yield return wfs;
+        }
+    }
+    
+    public void Instance()
+    {
+        var location = transform.position;
+        var newObj = Instantiate(prefab);
+    }
+}
